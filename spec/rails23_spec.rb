36bdbacb254d6d5370e03f1c94fa40d9f7a8c415
Terence Lee <hone02@gmail.com>
2015-09-04 12:31:16 -0400

    wip

--- spec/rails23_spec.rb
+++ spec/rails23_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 describe "Rails 2.3.x" do
   it "should deploy on ruby 1.8.7 on cedar" do
     app = Hatchet::Runner.new('rails23_mri_187').setup!
-    app.heroku.put_stack(app.name, "cedar")
+    app.heroku.put_stack(app.name, "cedar-14")
     app.deploy do |app, heroku|
       expect(successful_body(app)).to eq("hello")
     end
